fa1a67cfb8a6495b51285ac94a39715d2fdad4cf
oscardhc <oscardhc@sjtu.edu.cn>
2019-03-03 20:05:43 +0800

    Initial Commit

---
+++ STLite/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STLite
+//
+//  Created by Haichen Dong on 2019/3/3.
+//  Copyright © 2019 Haichen Dong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
